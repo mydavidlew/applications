f4f01efa352bed16ddffa495958db31598115d45
davidlew <12636609+mydavidlew@users.noreply.github.com>
2020-09-19 23:02:13 +0800

    Update conky-startup.sh

--- conky/conky-startup.sh
+++ conky/conky-startup.sh
@@ -1,4 +1,10 @@
 # No widgets enabled!
 # cmdline => conky -c /etc/conky/conky.conf
+#
+# Sensors Information
+# $sudo apt-get install lm-sensors
+# $sudo apt-get install hwinfo
+# $sudo apt-get install sensord
+# $sudo apt-get install fancontrol
 
 exit 0
