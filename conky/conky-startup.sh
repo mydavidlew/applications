87563ab98b3c2c959cf99850058b94c38ec8d5e3
davidlew <12636609+mydavidlew@users.noreply.github.com>
2020-09-30 17:29:01 +0800

    Update conky-startup.sh

--- conky/conky-startup.sh
+++ conky/conky-startup.sh
@@ -2,8 +2,9 @@
 # cmdline => conky -c /etc/conky/conky.conf
 #
 # Sensors Information
-# $sudo apt install lm-sensors
+# $sudo apt install lm-sensors | $sensors-detect | $sensors
 # $sudo apt install hwinfo
+# $sudo apt install hardinfo | $hardinfo -rma devices.so | $hardinfo
 # $sudo apt install psensor
 # $sudo apt install sensord
 # $sudo apt install fancontrol
