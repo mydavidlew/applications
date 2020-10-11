9e05d3833fbb263962093541b2d4a36d7a992aab
davidlew <12636609+mydavidlew@users.noreply.github.com>
2020-10-12 00:46:22 +0800

    Update conky-startup.sh

--- conky/conky-startup.sh
+++ conky/conky-startup.sh
@@ -6,8 +6,16 @@
 # $sudo apt install hwinfo
 # $sudo apt install hardinfo | $hardinfo -rma devices.so | $hardinfo
 # $sudo apt install psensor
-# $sudo apt install sensord or $sudo snap install indicator-sensors
+# $sudo snap install indicator-sensors
 # $sudo apt install fancontrol
 # $sudo apt install i2c-tools | $sudo modprobe eeprom
+# $sudo apt install libcanberra-gtk-module libcanberra-gtk3-module
+
+# Open Source AMD Radeon Ubuntu Driver
+# $sudo add-apt-repository ppa:oibaf/graphics-drivers
+# $sudo apt update && sudo apt -y upgrade
+# $sudo apt install mesa-utils | $glxinfo -B
+
+# $sudo apt install cpu-x
 
 exit 0
