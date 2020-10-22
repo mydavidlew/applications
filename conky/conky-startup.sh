89c697340634752e6d7f8444306cd03a7d69fd75
davidlew <12636609+mydavidlew@users.noreply.github.com>
2020-10-22 16:14:00 +0800

    Update conky-startup.sh

--- conky/conky-startup.sh
+++ conky/conky-startup.sh
@@ -1,22 +1,5 @@
 # No widgets enabled!
 # cmdline => conky -c /etc/conky/conky.conf
 #
-# Sensors Information
-# $sudo apt install lm-sensors | $sensors-detect | $sensors
-# $sudo apt install hwinfo
-# $sudo apt install hardinfo | $hardinfo -rma devices.so | $hardinfo
-# $sudo apt install psensor
-# $sudo snap install indicator-sensors
-# $sudo apt install fancontrol
-# $sudo apt install i2c-tools | $sudo modprobe eeprom
-# $sudo apt install libcanberra-gtk-module libcanberra-gtk3-module
-
-# Open Source Intel and AMD GPU Ubuntu Driver
-# $sudo add-apt-repository ppa:oibaf/graphics-drivers
-# $sudo apt update && sudo apt upgrade -y
-# $sudo apt dist-upgrade
-# $sudo apt install mesa-utils | $glxinfo -B
-
-# $sudo apt install cpu-x
 
 exit 0
