e03f979741969dc1fb75fcd007b41a186f6cd940
davidlew <12636609+mydavidlew@users.noreply.github.com>
2026-05-29 18:40:49 +0800

    Update conky-startup.sh

--- conky/conky-startup.sh
+++ conky/conky-startup.sh
@@ -1,4 +1,6 @@
 # No widgets enabled!
+name => Conky Sensor
+comment => Conky Monitoring
 cmdline => $sudo apt install conky-all
 cmdline => $conky --daemonize --pause=5 -c /etc/conky/conky.conf
 exit 0
